55ce77ce047e6c4b757356c2621672de1d6e8692
Jon Suderman <suderman@gmail.com>
2015-05-25 17:18:36 -0600

    SSL

--- api/db/config.js
+++ api/db/config.js
@@ -1,16 +1,31 @@
 // Configure database
+var fs = require('fs');
 module.exports = function(app) {
+
+  var connection = {
+    host:       process.env.DB_HOST || '127.0.0.1',
+    user:       process.env.DB_USER || 'root',
+    database:   process.env.DB_NAME || 'homemaker',
+    charset: 'utf8'
+  }
+  if (process.env.DB_PASS)       connection.password   = process.env.DB_PASS;
+
+  var ssl = {};
+  if (process.env.DB_CA) {
+    if (process.env.DB_CA)         ssl.ca         = fs.readFileSync(process.env.DB_CA);
+    if (process.env.DB_PFX)        ssl.pfx        = fs.readFileSync(process.env.DB_PFX);
+    if (process.env.DB_KEY)        ssl.key        = fs.readFileSync(process.env.DB_KEY);
+    if (process.env.DB_PASSPHRASE) ssl.passphrase = process.env.DB_PASSPHRASE;
+    if (process.env.DB_CERT)       ssl.cert       = fs.readFileSync(process.env.DB_CERT);
+    if (process.env.DB_CRL)        ssl.crl        = fs.readFileSync(process.env.DB_CRL);
+    if (process.env.DB_CIPHERS)    ssl.ciphers    = process.env.DB_CIPHERS;
+    connection.ssl = ssl;
+  }
 
   var knex = require('knex')({
     client: 'mysql',
     debug: false,
-    connection: {
-      host:     process.env.DB_HOST || '127.0.0.1',
-      user:     process.env.DB_USER || 'root',
-      password: process.env.DB_PASS || '',
-      database: process.env.DB_NAME || 'homemaker',
-      charset: 'utf8'
-    }
+    connection: connection
   });
 
   var bookshelf = require('bookshelf')(knex)
